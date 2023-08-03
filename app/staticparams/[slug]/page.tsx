e66a17dc76cdb8fdf335fcc8df787a4e13fb5b76
Your Name <you@example.com>
2023-08-03 22:34:56 +0530

    staic params lowercase

--- app/staticparams/[slug]/page.tsx
+++ app/staticparams/[slug]/page.tsx
@@ -1,15 +1,15 @@
+export default function Page({ params }: { params: { slug: string } }) {
+  return <div>My user: {params.slug}</div>;
+}
+
 export async function generateStaticParams() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users").then(
     (res) => res.json()
   );
 
   return users.map((user: any) => ({
-    slug: user.username,
+    slug: user.username.toString().toLowerCase(),
   }));
 }
 
 export const dynamicParams = false;
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My user: {params.slug}</div>;
-}
